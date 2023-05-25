27472f63ba3436f7135b75e1872e39af15ea5ab7
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-05-26 01:29:19 +0200

    update

--- AplicaciónWebLibre/AplicaciónWebLibre/wwwroot/js/main.js
+++ AplicaciónWebLibre/AplicaciónWebLibre/wwwroot/js/main.js
@@ -1,45 +1,17 @@
-﻿var URL = "https://api.spotify.com/v1/albums/";
+﻿var URL = "https://hp-api.onrender.com/api/characters";
 
-var albumsIds = [
-	{
-		//nombre: "Un Verano Sin Ti"
-		id: "3RQQmkQEvNCY4prGKE6oc5"
-	},
-	{
-		//nombre: "Los Niños del Parque",
-		id: "6hkJjr8eUF53zDqxXcc4Lf"
-	},
-	{
-		//nombre: "Microdosis",
-		id: "0QLDQG7Jx78rEUDW03IhHC"
-	},
-	{
-		//nombre: "YHLQMDLG",
-		id: "5lJqux7orBlA1QzyiBGti1"
-	},
-	{
-		//nombre: "Saturno",
-		id: "5AcRssiG0Zqu3lqYW7hMoM"
-	},
-	{
-		//nombre: "Feliz cumpleaños Ferxxo",
-		id: "7pijRxgRaBirPz6wDaJIp9"
-	},
-	{
-		//nombre: "Juglar del Siglo XXI",
-		id: "7bxbscLnwPLah6i2bZVA9V"
-	},
-	{
-		//nombre: "Muerte",
-		id: "27xqCLyTHom0wyjtw08K12"
+//function executeOnloadMethod() {
+//    DotNet.invokeMethodAsync("AplicaciónWebLibre", "GetHpCharacters")
+//        //.catch(error => {
+//        //    console.error(error);
+//        //});
+//}
+
+export async function getHpCharacters() {
+	var request = await fetch(URL);
+	var data = await request.json();
+
+	for (var i = 0; i < 25; i++) {
+		console.log(data[i]);
 	}
-];
-
-export async function getAlbums() {
-	for (var i = 0; i < albumsIds.length; i++) {
-		var request = await fetch(URL + albumsIds[i].id);
-		var data = await request.json();
-
-		console.log(data);
-    }
 }
