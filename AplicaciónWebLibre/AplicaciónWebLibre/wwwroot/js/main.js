4697727850264e67bd1fb175a14a5701473dcc6f
Juanma <119064242+JuanmaBenitez73@users.noreply.github.com>
2023-05-26 03:14:25 +0200

    update

--- AplicaciónWebLibre/AplicaciónWebLibre/wwwroot/js/main.js
+++ AplicaciónWebLibre/AplicaciónWebLibre/wwwroot/js/main.js
@@ -1,12 +1,5 @@
 ﻿var URL = "https://hp-api.onrender.com/api/characters";
 
-//function executeOnloadMethod() {
-//    DotNet.invokeMethodAsync("AplicaciónWebLibre", "GetHpCharacters")
-//        //.catch(error => {
-//        //    console.error(error);
-//        //});
-//}
-
 export async function getHpCharacters() {
 	var request = await fetch(URL);
 	var data = await request.json();
